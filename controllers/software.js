d05393ddf5d337bdc1e9a02eeb4e09fdce920887
knowlage <febmarich@gmail.com>
2020-07-16 15:49:17 +0700

    2020/7/16

--- controllers/software.js
+++ controllers/software.js
@@ -53,4 +53,81 @@ router.get('/getSoftwareByEmployeenumber/:employeenumber?', (req, res) => {
 
 })
 
+router.get('/getSoftwaresList', (req, res) => {
+    let sql = 'call Softwares_get_softwares()'
+    conn.getConnection((err, connection) => {
+        if(err){
+            res.status(400).json({"code":0, "message":err})
+        }
+        connection.query(sql, (err, rs, field) => {
+            connection.release()
+            if(err){
+                res.status(400).json({"code":0, "message":err})
+            }else{
+                if(rs[0].length > 0){
+                    res.status(200).json({"code":1, "message":"Access Complete", "data":rs[0]})
+                }else{
+                    res.status(200).json({"code":0, "message":"Data Not Found."})
+                }
+            }
+        })
+    })
+})
+
+function putUpdate(req, res, sql, params){
+    conn.getConnection((err, connection) => {
+        if(err){
+            res.status(400).json({"code":0, "message":err})
+        }
+        connection.query(sql, params, (err, rs, field) => {
+            connection.release()
+            if(err){
+                res.status(400).json({"code":1, "message":err})
+            }else{
+                if(rs['affectedRows'] == 1){
+                    res.status(200).json({"code":1, "message":"Update Complete"})
+                }else{
+                    res.status(200).json({"code":0, "message":"Update Fail"})
+                }
+            }
+        })
+    })
+}
+
+router.put('/pinDeactivateSoftware/:id', (req, res) => {
+    let id = req.params.id
+    let sql = 'call Software_deactivate(?)'
+    putUpdate(req, res, sql, id)
+    
+})
+
+router.put('/pinActivateSoftware/:id', (req, res) => {
+    let id = req.params.id
+    let sql = 'call Software_activate(?)'
+    putUpdate(req, res, sql, id)
+})
+
+router.delete('/deleteSoftware/:id', (req, res) => {
+    let sql = 'call Software_delete(?)'
+    let id = req.params.id
+
+    conn.getConnection((err, connection) => {
+        if(err){
+            res.status(400).json({"code":0, "message":err})
+        }
+        connection.query(sql, (err, rs, field) => {
+            connection.release()
+            if(err){
+                res.status(400).json({"code":0, "message":err})
+            }else{
+                if(rs['affectedRows'] == 1){
+                    res.status(200).json({"code":1, "message":"Delete Success"})
+                }else{
+                    res.status(200).json({"code":0, "message":"Delete Fail"})
+                }
+            }
+        })
+    })
+})
+
 module.exports = router
